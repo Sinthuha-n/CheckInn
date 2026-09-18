338c83623572515c202d17d2f99bd8f8647d278a
Sinthuha-n <nadasinthu09@gmail.com>
2026-09-18 13:50:16 +0530

    feat: add user registration API

---
+++ checkinn-backend/src/main/java/com/checkinn/service/AuthService.java
@@ -0,0 +1,32 @@
+package com.checkinn.service;
+
+import com.checkinn.dto.RegisterRequest;
+import com.checkinn.entity.User;
+import com.checkinn.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User register(RegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already registered");
+        }
+
+        User user = new User();
+
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(request.getPassword());
+        user.setRole("USER");
+
+        return userRepository.save(user);
+    }
+}
